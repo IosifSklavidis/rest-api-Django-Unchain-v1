d1d993ae56871981e7545039795ce6b5b8d1096e
isklavidis <isklavidis@konnektable.com>
2020-11-27 14:45:13 +0200

    django admin

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from profiles_api import models
+
+# this enables the user to register as a admin
+admin.site.register(models.UserProfile)
